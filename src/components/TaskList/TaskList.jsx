d83fd6deef0143fd90c326e420c75f5e6baef03a
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2025-03-25 12:21:18 +0530

    create employee dashboard

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -11,7 +11,44 @@ const TaskList = () => {
           <h3 className="bg-red-600 px-4 py-1 rounded-md">High</h3>
           <h4>20 feb 2024</h4>
         </div>
-        <h2 className="mt-5 text-xl font-semibold">Make a youtube video</h2>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi autem
+          hic neque voluptas
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-[#E69DB8]  rounded-xl flex-shrink-0 p-2">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-4 py-1 rounded-md">High</h3>
+          <h4>20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi autem
+          hic neque voluptas
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-[#E69DB8]  rounded-xl flex-shrink-0 p-2">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-4 py-1 rounded-md">High</h3>
+          <h4>20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi autem
+          hic neque voluptas
+        </p>
+      </div>
+      <div className="h-full w-[300px] bg-[#E69DB8]  rounded-xl flex-shrink-0 p-2">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 px-4 py-1 rounded-md">High</h3>
+          <h4>20 feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi autem
+          hic neque voluptas
+        </p>
       </div>
     </div>
   );
