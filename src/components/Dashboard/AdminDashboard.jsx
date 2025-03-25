68c2bd454bba1eaeb880b5f4ad9df3af3cda98c1
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2025-03-25 17:32:14 +0530

    create admin dashboard ui

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,72 +1,14 @@
 import React from "react";
 import Header from "../other/Header";
+import CreateTask from "../other/CreateTask";
+import AllTask from "../other/AllTask";
 
 const AdminDashboard = () => {
   return (
     <div className="h-screen w-full p-7">
       <Header />
-      <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
-        <form
-          action=""
-          className="flex items-start justify-between flex-wrap   w-full"
-        >
-          <div className="w-1/2 ">
-            <div className="">
-              <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Make a task"
-                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
-              />
-            </div>
-
-            <div>
-              <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Make a task"
-                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
-              />
-            </div>
-            <div>
-              <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Make a task"
-                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
-              />
-            </div>
-            <div>
-              <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Make a task"
-                className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
-              />
-            </div>
-          </div>
-          <div className="w-2/5 flex flex-col items-start ">
-            <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
-            <textarea
-              name=""
-              id=""
-              className=" w-full h-44 text-sm bg-transparent border-[1px] border-gray-400 outline-none rounded px-4 py-2"
-            ></textarea>
-
-            <button className="bg-green-700 py-3 px-5 text-sm mt-4 w-full  rounded-md hover:bg-white hover:text-green-700">
-              Create Task
-            </button>
-          </div>
-        </form>
-      </div>
+      <CreateTask />
+      <AllTask />
     </div>
   );
 };
---
+++ src/components/other/AllTask.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div
+      className="bg-[#1c1c1c] h-48 overflow-auto p-5 rounded mt-5"
+      id="tasklist"
+    >
+      <div className="bg-red-400 px-4 py-2 flex justify-between rounded  mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+      <div className="bg-green-400 px-4 py-2 flex justify-between rounded mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+      <div className="bg-yellow-400 px-4 py-2 flex justify-between rounded mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+      <div className="bg-purple-400 px-4 py-2 flex justify-between rounded mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+      <div className="bg-red-400 px-4 py-2 flex justify-between rounded  mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+      <div className="bg-green-400 px-4 py-2 flex justify-between rounded mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+      <div className="bg-yellow-400 px-4 py-2 flex justify-between rounded mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+      <div className="bg-purple-400 px-4 py-2 flex justify-between rounded mb-2">
+        <h2>Deepesh</h2>
+        <h3>Make a UI design</h3>
+        <h5>status</h5>
+      </div>
+    </div>
+  );
+};
+
+export default AllTask;
